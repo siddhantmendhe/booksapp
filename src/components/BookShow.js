5919e40d4774a2cfb802081db583e18c0dcc9657
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-01-29 20:49:05 +0530

    added images

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -17,7 +17,9 @@ function BookShow({book,onDelete,onEdit}) {
   if (showEdit) {
     content=<BookEdit book={book} onSubmit={handleSubmit} />;
   }
-  return <div className="book-show"><div>{content}</div>
+  return <div className="book-show">
+    <img src={`https://picsum.photos/seed/${book.id}/300/200`} alt="rondom"/>
+      <div>{content}</div>
   <div className="actions" >
     <button className="edit" onClick={handleEditClick}>Edit</button>
     <button className="delete" onClick={handleDeleteClick}>Delete</button>
